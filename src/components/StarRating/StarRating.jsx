f3b429e52a70164c13a8435c264f72c7d793a38a
Vimal Raveendran <vimalraveendra@gmail.com>
2021-12-08 22:14:28 +0100

    created containers folder & moved App component & its style sheet, also created components folder,ccreated StarRating component & its style sheet inside of it

---
+++ src/components/StarRating/StarRating.jsx
@@ -0,0 +1,11 @@
+import "./StarRating.css"
+
+const starRating= ()=>{
+    return(
+        <div>
+          <h3>Rating</h3>
+        </div>
+    )
+}
+
+export default starRating;
--- src/contianers/App.js
+++ src/contianers/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import StarRating from '../components/StarRating/StarRating';
 
 function App() {
   return (
     <div className="App">
       <h1>Star Rating</h1>
+      <StarRating />
     </div>
   );
 }
